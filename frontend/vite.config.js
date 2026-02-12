4b21d752cda88438f90fb1c9c309e6dae94b1f44
Ebenenezer Gasonoo <iamknightrae@gmail.com>
2026-02-12 20:33:51 +0200

    Final Change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Easy-Intern/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/EasyIntern/' : '/',
   server: {
     port: 3000,
     proxy: {
